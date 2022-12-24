0e4ad9804604a1835cd3aa1baba223fc216b280f
Buckmein <dodin.dima@gmail.com>
2022-12-24 20:38:16 +0300

    Дописать enemy_ships и реализацию ходов

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 battlefield = [[]]
 
 
@@ -5,10 +7,18 @@ def new_game():
     """
     Функция начала новой игры 
     """
-    ship = []
-    ostatok = [4, 2, 1]
     clear_field()
     show_field()
+    ship_input()
+
+
+def ship_input():
+    """
+    Ввод кораблейигрока
+    :return:
+    """
+    ships = []
+    ostatok = [4, 2, 1]
     for i in range(7):
         print(f"У вас осталось: {ostatok[2]} Трехмачтовых, {ostatok[1]} Двумачтовых и {ostatok[0]} Одномачтовых кораблей\n")
         print("Введите тип корабля который хотите разместить")
@@ -23,9 +33,47 @@ def new_game():
             ship_cords += " " + input()
         ship_cords = ship_cords.split()
         print(ship_cords)
-        ship.append(Ship(ship_type, *ship_cords))
+        ships.append(Ship(ship_type, *ship_cords))
         ostatok[int(ship_type)-1] -= 1
         show_field()
+
+
+def enemy_ships():
+    """
+    Создание кораблей опонента ДОПИСАТЬ
+    :return:
+    """
+    e_ships = []
+    e_ships.clear()
+    x0 = random.randint(0, 5)
+    if x0 > 4:
+        x1 = x0-2
+        moved = True
+    elif x0 < 3:
+        x1 = x0+2
+        moved = True
+    y0 = random.randint(0, 5)
+    if y0 >= 3 and not moved:
+        y1 = y0-2
+    elif not moved:
+        y1 = y0+2
+    e_ships.append(Ship(3, x0, y0, x1, y1))
+    for i in range(2):
+        x0 = random.randint(0, 5)
+        y0 = random.randint(0, 5)
+        if x0 > 3:
+            x1 = x0 - 2
+            moved = True
+        elif x0 < 2:
+            x1 = x0 + 2
+            moved = True
+        if y0 >= 2 and not moved:
+            y1 = y0 - 2
+        elif not moved:
+            y1 = y0 + 2
+        e_ships.append(Ship(2, x0, y0, x1, y1))
+
+
 
 
 def show_field():
@@ -54,7 +102,7 @@ def clear_field():
 
 def game():
     """
-    Реализация ходов
+    Реализация хода игры
     :return:
     """
 
@@ -67,12 +115,20 @@ class Ship:
     def __init__(self, ship_type, x0, y0, x1=0, y1=0):
         cords_begin = x0, y0
         cords_end = x1, y1
-        type = ship_type
+        size = ship_type
 
     @property
     def get_cords(self):
         return self.cords_begin, self.cords_end
 
 
+    @property
+    def get_type(self):
+        return self.size
+
+
+'''
+■
+'''
 new_game()
 
