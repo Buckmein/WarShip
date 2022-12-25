d41b60f7078dea2349e71a9a64d951a57cb95dcc
Buckmein <dodin.dima@gmail.com>
2022-12-25 18:38:28 +0300

    Переписать проверку места для корабля и реализацию ходов

--- main.py
+++ main.py
@@ -15,34 +15,49 @@ def new_game():
 
 def ship_input():
     """
-    Ввод кораблейигрока
+    Ввод кораблей игрока
     :return:
     """
     ships = []
+    ships.clear()
     ostatok = [4, 2, 1]
     for i in range(7):
         type_check = True
-        print(
-            f"У вас осталось: {ostatok[2]} Трехмачтовых, {ostatok[1]} Двумачтовых и {ostatok[0]} Одномачтовых кораблей\n")
+        cords_check = True
         while type_check:
+            print(
+            f"У вас осталось: {ostatok[2]} Трехмачтовых, {ostatok[1]} Двумачтовых и {ostatok[0]} Одномачтовых кораблей")
             print("Введите тип корабля который хотите разместить")
             ship_type = input()
             if ship_type in ('1', '2', '3') and ostatok[int(ship_type) - 1] != 0:
                 type_check = False
             else:
-                print("Нет такого типа кораблей")
-        if ship_type == "1":
-            print("Введите координаты через пробел")
-            ship_cords = input()
-        else:
-            print("Введите координаты начала через пробел")
-            ship_cords = input()
-            print("Введите координаты конца через пробел")
-            ship_cords += " " + input()
-        ship_cords = map(int, ship_cords.split())
-        ships.append(Ship(ship_type, *ship_cords))
+                print("!!!Нет такого типа кораблей!!!")
+        while cords_check:
+            if ship_type == "1":
+                print("Введите координаты через пробел")
+                ship_cords = input()
+                cords0 = map(int, ship_cords.split())
+                cords1 = cords0
+            else:
+                print("Введите координаты начала через пробел")
+                ship_cords = input()
+                cords0 = map(int, ship_cords.split())
+                print("Введите координаты конца через пробел")
+                ship_cords = input()
+                cords1 = map(int, ship_cords.split())
+            if len(ships) == 0:
+                cords_check = False
+            for j in range(len(ships)):
+                if cords0 in ships[j].get_space or cords1 in ships[j].get_space \
+                        or cords0 in ships[j].get_parts or cords1 in ships[j].get_parts:
+                    print("!!!корабль не вмещается, попробуйте другое место!!!")
+                else:
+                    cords_check = False
+
+        ships.append(Ship(ship_type, *cords0, *cords1))
         ostatok[int(ship_type) - 1] -= 1
-        show_field()
+        show_field(ships)
 
 
 def enemy_ships():
@@ -81,7 +96,7 @@ def enemy_ships():
             elif not moved:
                 y1 = y0 + 2
             for i in range(len(e_ships)):
-                if [x0, y0] not in e_ships[i].get_parts or [x0, y0] not in e_ships[i].get_space or\
+                if [x0, y0] not in e_ships[i].get_parts or [x0, y0] not in e_ships[i].get_space or \
                         [x1, y1] not in e_ships[i].get_parts or [x1, y1] not in e_ships[i].get_space:
                     space_check = False
         e_ships.append(Ship(2, int(x0), int(y0), int(x1), int(y1)))
@@ -96,11 +111,24 @@ def enemy_ships():
         e_ships.append(1, int(x0), int(y0))
 
 
-def show_field():
+def show_field(ships=[]):
     """
     Функциявывода поля на экран
     """
     print("  | 1 | 2 | 3 | 4 | 5 | 6 |")
+    if ships != 0:
+        for i in range(len(ships)):
+            cords = ships[i]
+            for j in range(len(cords.get_space)):
+                cord_x = int(cords.get_space[j][0]) - 1
+                cord_y = int(cords.get_space[j][1]) - 1
+                if cord_x >= 0 and cord_y >= 0:
+                    battlefield[cord_x][cord_y] = '●'
+            for j in range(len(cords.get_parts)):
+                cord_x = int(cords.get_parts[j][0]) - 1
+                cord_y = int(cords.get_parts[j][1]) - 1
+                if cord_x >=0 and cord_y >= 0:
+                    battlefield[cord_x][cord_y] = '■'
     for i in range(6):
         stroka = ''
         stroka += f"{i + 1} |"
@@ -117,7 +145,7 @@ def clear_field():
     for i in range(6):
         battlefield.append([])
         for j in range(6):
-            battlefield[i].append('O')
+            battlefield[i].append('◯')
 
 
 def game():
@@ -129,28 +157,28 @@ def game():
 
 class Ship:
     """
-    Класс создания кораблей хранит делает
+    Класс создания кораблей хранит жизни, координтаны корабля и пустого пространства вокруг,
     """
 
     def __init__(self, ship_type, x0, y0, x1=0, y1=0):
-        parts = [[]]  # Части корабля
-        space = []  # Пространство между кораблями
-        size = ship_type
-        health = int(size)
-        if size == '3':
-            parts = [[x0, y0], [int(x0 + x1) // 2, int(y0 + y1) // 2], [x1, y1]]
-        elif size == '2':
-            parts = [[x0, y0], [x1, y1]]
+        self.parts = []  # Части корабля
+        self.space = []  # Пространство между кораблями
+        self.size = ship_type
+        self.health = int(self.size)
+        if self.size == '3':
+            self.parts = [[x0, y0], [int(x0 + x1) // 2, int(y0 + y1) // 2], [x1, y1]]
+        elif self.size == '2':
+            self.parts = [[x0, y0], [x1, y1]]
         else:
             print(1)
-            parts = [[x0, y0]]
-        for i in range(len(parts)):
+            self.parts = [[x0, y0]]
+        for i in range(len(self.parts)):
             delta_y = -2
             for j in range(3):
                 delta_x = -1
                 delta_y += 1
                 for k in range(3):
-                    space.append([int(parts[i][0]) + delta_x, int(parts[i][1]) + delta_y])
+                    self.space.append([int(self.parts[i][0]) + delta_x, int(self.parts[i][1]) + delta_y])
                     delta_x += 1
 
     def hit(self, x, y):
@@ -192,6 +220,6 @@ class Ship:
 
 
 '''
-■
+■  ◉ ◯
 '''
 new_game()
