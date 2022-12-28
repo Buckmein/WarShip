e402c9b9259daddbf3e426cd1406dab03cea878b
Dmitriy Dodin <dodin.dima@gmail.com>
2022-12-28 22:08:20 +0300

    дописана генерация вражеских кораблей, написать ход игры

--- main.py
+++ main.py
@@ -21,12 +21,16 @@ def ship_input():
     ships = []
     ships.clear()
     ostatok = [4, 2, 1]
-    for i in range(7):
+    ship_type = ''  # Тип корабля
+    cords0 = []  # Координаты начала
+    cords1 = []  # Координаты конца
+    for si in range(7):
         type_check = True
         cords_check = True
         while type_check:
             print(
-            f"У вас осталось: {ostatok[2]} Трехмачтовых (3), {ostatok[1]} Двумачтовых (2) и {ostatok[0]} Одномачтовых кораблей (1)")
+                f'У вас осталось: {ostatok[2]} Трехмачтовых (3), {ostatok[1]} Двумачтовых (2) и {ostatok[0]} '
+                f'Одномачтовых кораблей (1)')
             print("Введите тип корабля который хотите разместить")
             ship_type = input()
             if ship_type in ('1', '2', '3') and ostatok[int(ship_type) - 1] != 0:
@@ -54,11 +58,11 @@ def ship_input():
                 except ValueError:
                     print("!!!Неверный формат ввода!!!")
                     continue
-                if ((int(abs(cords0[0]-cords1[0])) != int(ship_type)-1)
-                        and (int((abs(cords0[1]-cords1[1])) != int(ship_type)-1)))\
-                        or (cords1[0] not in range(1, 7)) or (cords1[1] not in range(1, 7))\
-                        or (cords0[0] not in range(1, 7)) or (cords0[1] not in range(1, 7))\
-                        or all([cords0[0]-cords1[0] != 0, cords0[1]-cords1[1] != 0]):
+                if ((int(abs(cords0[0] - cords1[0])) != int(ship_type) - 1)
+                    and (int((abs(cords0[1] - cords1[1])) != int(ship_type) - 1))) \
+                        or (cords1[0] not in range(1, 7)) or (cords1[1] not in range(1, 7)) \
+                        or (cords0[0] not in range(1, 7)) or (cords0[1] not in range(1, 7)) \
+                        or all([cords0[0] - cords1[0] != 0, cords0[1] - cords1[1] != 0]):
                     print("!!!Неверно указаны координаты!!!")
                     continue
             if len(ships) == 0:
@@ -78,7 +82,7 @@ def ship_input():
 
 def enemy_ships():
     """
-    Создание кораблей опонента ДОПИСАТЬ
+    Создание кораблей оппонента ДОПИСАТЬ
     :return:
     """
     e_ships = []
@@ -103,10 +107,14 @@ def enemy_ships():
     else:
         y1 = y0
     e_ships.append(Ship(3, int(x0), int(y0), int(x1), int(y1)))
-    for i in range(2):
+    for si in range(2):
         space_check = True
         while space_check:
             space_check1 = False
+            tries += 1
+            if tries > 999:
+                enemy_ships()
+                return None
             x0 = random.randint(1, 6)
             y0 = random.randint(1, 6)
             if x0 > 3:
@@ -125,17 +133,21 @@ def enemy_ships():
             else:
                 y1 = y0
             for j in range(len(e_ships)):
-                if ([x0, y0] not in e_ships[j].get_parts) and ([x0, y0] not in e_ships[j].get_space)\
+                if ([x0, y0] not in e_ships[j].get_parts) and ([x0, y0] not in e_ships[j].get_space) \
                         and ([x1, y1] not in e_ships[j].get_parts) and ([x1, y1] not in e_ships[j].get_space):
                     space_check = False
                 else:
                     space_check1 = True
             space_check = space_check1 or space_check
         e_ships.append(Ship(2, int(x0), int(y0), int(x1), int(y1)))
-    for i in range(4):
+    for si in range(4):
         space_check = True
         while space_check:
+            tries += 1
             space_check1 = False
+            if tries > 999:
+                enemy_ships()
+                return None
             x0 = random.randint(1, 6)
             x1 = x0
             y0 = random.randint(1, 6)
@@ -147,31 +159,40 @@ def enemy_ships():
                     space_check1 = True
             space_check = space_check1 or space_check
         e_ships.append(Ship(1, int(x0), int(y0), int(x1), int(y1)))
-    show_field(e_ships)
+    #show_field(e_ships)
 
 
-def show_field(ships=[]):
+def show_field(ships=None):
     """
-    Функциявывода поля на экран
+    Функция вывода поля на экран
     """
+    clear_field()
+    if ships is None:
+        ships = []
     print("  | 1 | 2 | 3 | 4 | 5 | 6 |")
-    for i in range(len(ships)):
-        cords = ships[i]
+    for si in range(len(ships)):
+        cords = ships[si]
         for j in range(len(cords.get_space)):
             cord_x = int(cords.get_space[j][0]) - 1
             cord_y = int(cords.get_space[j][1]) - 1
-            if cord_x in range(0, 6) and cord_y in range(0, 6):
-                battlefield[cord_x][cord_y] = '●'
+            try:
+                if cord_x in range(0, 6) and cord_y in range(0, 6):
+                    battlefield[cord_x][cord_y] = '◦'
+            except IndexError:
+                print(cord_x, cord_y)
         for j in range(len(cords.get_parts)):
             cord_x = int(cords.get_parts[j][0]) - 1
             cord_y = int(cords.get_parts[j][1]) - 1
-            if cord_x in range(0, 6) and cord_y in range(0, 6):
-                battlefield[cord_x][cord_y] = '■'
-    for i in range(6):
+            try:
+                if cord_x in range(0, 6) and cord_y in range(0, 6):
+                    battlefield[cord_x][cord_y] = '■'
+            except IndexError:
+                print(cord_x, cord_y)
+    for si in range(6):
         stroka = ''
         stroka += f"{i + 1} |"
         for j in range(6):
-            stroka += f' {battlefield[i][j]} |'
+            stroka += f' {battlefield[si][j]} |'
         print(stroka)
     print('*******************')
 
@@ -181,10 +202,10 @@ def clear_field():
     Функция очистки игрового поля
     """
     battlefield.clear()
-    for i in range(6):
+    for si in range(6):
         battlefield.append([])
         for j in range(6):
-            battlefield[i].append('◯')
+            battlefield[si].append('◯')
 
 
 def game():
@@ -196,7 +217,7 @@ def game():
 
 class Ship:
     """
-    Класс создания кораблей хранит жизни, координтаны корабля и пустого пространства вокруг,
+    Класс создания кораблей хранит жизни, координатны корабля и пустого пространства вокруг,
     """
 
     def __init__(self, ship_type, x0, y0, x1=0, y1=0):
@@ -210,18 +231,18 @@ class Ship:
             self.parts = [[x0, y0], [x1, y1]]
         else:
             self.parts = [[x0, y0]]
-        for i in range(len(self.parts)):
+        for si in range(len(self.parts)):
             delta_y = -2
             for j in range(3):
                 delta_x = -1
                 delta_y += 1
                 for k in range(3):
-                    self.space.append([int(self.parts[i][0]) + delta_x, int(self.parts[i][1]) + delta_y])
+                    self.space.append([int(self.parts[si][0]) + delta_x, int(self.parts[si][1]) + delta_y])
                     delta_x += 1
         for j in range(self.size):
-            for i in self.space:
-                if (self.space.count(i) > 1) or (i[0] > 6) or (i[1] > 6):
-                    self.space.remove(i)
+            for si in self.space:
+                if (self.space.count(si) > 1) or (si[0] > 6) or (si[1] > 6):
+                    self.space.remove(si)
 
     def hit(self, x, y):
         if [x, y] in self.get_parts:
@@ -265,8 +286,5 @@ class Ship:
 ■  ◉ ◯
 '''
 
-#new_game()
+new_game()
 
-for i in range(10):
-    clear_field()
-    enemy_ships()
